a16e419a3075c9470be32cb38831386bb55069f8
ranvane <ranvane@gmail.com>
2025-03-16 22:46:16 +0800

    只获取英文和中文壁纸，修复当支持的语言太多时导致github actions执行时长过长而失败

--- python/github_workflows_update.py
+++ python/github_workflows_update.py
@@ -1,14 +1,15 @@
+import os
+
 from generate_month_json_data import year_month_langs
 from generate_yearly_md import process_index_template_folder, generate_index_md
 from update_bing_json import update_lang_data
-import os
 
 # 判断是否在 GitHub Workflows 中运行
 is_github_workflow = os.getenv('GITHUB_ACTIONS')
 # 支持的语言列表（ROW为通用地区）
-LANGS = ['ROW', 'en-US', 'en-CA', 'en-GB', 'en-IN', 'es-ES',
-         'fr-FR', 'fr-CA', 'it-IT', 'ja-JP', 'pt-BR', 'de-DE', 'zh-CN']
-
+# LANGS = ['ROW', 'en-US', 'en-CA', 'en-GB', 'en-IN', 'es-ES',
+#          'fr-FR', 'fr-CA', 'it-IT', 'ja-JP', 'pt-BR', 'de-DE', 'zh-CN']
+LANGS = ['en-US', 'zh-CN']
 if is_github_workflow:
     print("Running in GitHub Workflows")
     # 更新所有的地区的最近days天的bing桌面数据
